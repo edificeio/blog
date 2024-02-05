0691d4f8c9f607aa8547f90952dcf4d381145ff8
Romu-C <romuald.coulaud@gmail.com>
2024-02-05 16:31:51 +0100

    chore: Remove store for this PR

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,24 +1,24 @@
-import { create } from "zustand";
+// import { create } from "zustand";
 
-import { Post } from "../models/post";
+// import { Post } from "../models/post";
 
-interface State {
-  posts: Post[];
-  updaters: {
-    setPosts: (posts: Post[]) => void;
-  };
-}
+// interface State {
+//   posts: Post[];
+//   updaters: {
+//     setPosts: (posts: Post[]) => void;
+//   };
+// }
 
-export const useStoreContext = create<State>()((set) => ({
-  blog: undefined,
-  posts: [],
-  updaters: {
-    setPosts: (posts) => set({ posts }),
-  },
-}));
+// export const useStoreContext = create<State>()((set) => ({
+//   blog: undefined,
+//   posts: [],
+//   updaters: {
+//     setPosts: (posts) => set({ posts }),
+//   },
+// }));
 
-export const usePosts = () => useStoreContext((state) => state.posts);
+// export const usePosts = () => useStoreContext((state) => state.posts);
 
-/* Export updaters */
-export const useStoreUpdaters = () =>
-  useStoreContext((state) => state.updaters);
+// /* Export updaters */
+// export const useStoreUpdaters = () =>
+//   useStoreContext((state) => state.updaters);
