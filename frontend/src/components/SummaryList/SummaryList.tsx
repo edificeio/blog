f99cb422ac946225050950b76701b995586eb9ef
Romu-C <romuald.coulaud@gmail.com>
2024-02-06 14:30:58 +0100

    feat: #WB-2338 Add onClick action

--- frontend/src/components/SummaryList/SummaryList.tsx
+++ frontend/src/components/SummaryList/SummaryList.tsx
@@ -1,10 +1,20 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { useOdeClient } from "@edifice-ui/react";
 
 import { dayjs } from "~/utils/dayjs";
 
 // TODO Move to edifice-ui (don't forget css) css
+
 export type SummaryListProps = {
+  /**
+   * Items to display
+   */
   list: SummaryListObject[];
+
+  /**
+   * Action on click
+   */
+  onClick?: (item: SummaryListObject) => void;
 };
 
 export type SummaryListObject = {
@@ -13,7 +23,7 @@ export type SummaryListObject = {
   date: string;
 };
 
-export const SummaryList = ({ list }: SummaryListProps) => {
+export const SummaryList = ({ list, onClick }: SummaryListProps) => {
   const { currentLanguage } = useOdeClient();
   const displayDate = (date: string) => {
     return dayjs(date)
@@ -21,10 +31,22 @@ export const SummaryList = ({ list }: SummaryListProps) => {
       .format("D MMM YYYY");
   };
 
+  const handleOnClick = (item: SummaryListObject) => {
+    onClick?.(item);
+  };
+
   return (
     <div className="pt-8">
       {list.map((item /*, index*/) => (
-        <div className="pb-8 d-flex summary-list-item" key={item.id}>
+        <div
+          className="pb-8 d-flex summary-list-item"
+          key={item.id}
+          onClick={() => {
+            handleOnClick(item);
+          }}
+          role="button"
+          tabIndex={0}
+        >
           <div className="summary-list-item-symbole text-primary">
             <div className="summary-list-item-symbole-circle"></div>
           </div>
